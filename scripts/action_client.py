d56a150f9ec5fb1ecb89e6edcb437927db8ffc6b
Marco <mark99961@gmail.com>
2025-01-02 16:15:01 +0000

    feedback and status info in the action client

--- scripts/action_client.py
+++ scripts/action_client.py
@@ -7,6 +7,7 @@ from nav_msgs.msg import Odometry
 import math
 import actionlib
 import actionlib.msg
+from actionlib_msgs.msg import GoalStatus
 import assignment2_rt_part1.msg
 from tf import transformations
 from std_srvs.srv import *
@@ -24,7 +25,7 @@ def goal_planning_client(t_pose):
     client = actionlib.SimpleActionClient('/reaching_goal', assignment2_rt_part1.msg.PlanningAction)
     client.wait_for_server()
     goal = assignment2_rt_part1.msg.PlanningGoal(target_pose=t_pose)
-    client.send_goal(goal)
+    client.send_goal(goal, done_cb=done_callback, feedback_cb=feedback_callback)
     
     #client.wait_for_result()
     
@@ -37,6 +38,27 @@ def goal_planning_client(t_pose):
         client.wait_for_result()
         
     return client.get_result()
+    
+def feedback_callback(feedback):
+    
+    actual_position = feedback.actual_pose.position
+   
+    target_position = pose.pose.position
+    
+    distance = math.sqrt(
+        (actual_position.x - target_position.x)**2 +
+        (actual_position.y - target_position.y)**2 +
+        (actual_position.z - target_position.z)**2
+    )
+    
+    rospy.loginfo("[FEEDBACK] Position: (%f, %f, %f); Distance: %f", actual_position.x, actual_position.y, actual_position.z, distance)
+
+        
+def done_callback(state, result):
+    
+    if state == GoalStatus.SUCCEEDED:
+        rospy.loginfo("[STATE] Goal reached!")
+        
     
 def main():
     global pose
