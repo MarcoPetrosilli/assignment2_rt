53b2019843d86ccf07273ce3c95e1f5546cf8aaa
Marco <mark99961@gmail.com>
2025-01-05 14:57:08 +0000

    change second node implementation in a service node

--- scripts/last_target_node.py
+++ scripts/last_target_node.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 import rospy
 from geometry_msgs.msg import PoseStamped
+from assignment2_rt_part1.srv import target_coord, target_coordResponse
 
 x = 0
 y = 0
@@ -11,19 +12,22 @@ def last_target_clbk(msg):
 	x = msg.pose.position.x
 	y = msg.pose.position.y
 	
+def return_values(req):
+    global x
+    global y
+
+    return SentCoordsResponse(x, y)
+	
 def main():
 	global x
 	global y
 	
 	rospy.init_node('last_target_node')
 	rospy.Subscriber('/last_target', PoseStamped, last_target_clbk)
-	rate = rospy.Rate(10)
-	
-	
-	while not rospy.is_shutdown():
-		print("x: %f, y: %f" % (x, y))
-		rate.sleep()
-	
+	rospy.Service('target_coord', target_coord, return_values) 
+    	rospy.spin()
+		
+
 
 if __name__ == "__main__":
-	main()
+    main()
