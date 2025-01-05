f2a17381a40c62f403633064e541edd1012df102
Marco <mark99961@gmail.com>
2025-01-05 15:17:28 +0000

    other fix to make the other node a service, service tested and it works

--- scripts/last_target_node.py
+++ scripts/last_target_node.py
@@ -3,31 +3,38 @@ import rospy
 from geometry_msgs.msg import PoseStamped
 from assignment2_rt_part1.srv import target_coord, target_coordResponse
 
+# Variabili globali per memorizzare le coordinate
 x = 0
 y = 0
 
+# Callback per il subscriber
 def last_target_clbk(msg):
-	global x
-	global y
-	x = msg.pose.position.x
-	y = msg.pose.position.y
-	
+    global x, y
+    x = msg.pose.position.x
+    y = msg.pose.position.y
+
+# Callback per il servizio
 def return_values(req):
-    global x
-    global y
+    global x, y
+    return target_coordResponse(x, y)
 
-    return SentCoordsResponse(x, y)
-	
 def main():
-	global x
-	global y
-	
-	rospy.init_node('last_target_node')
-	rospy.Subscriber('/last_target', PoseStamped, last_target_clbk)
-	rospy.Service('target_coord', target_coord, return_values) 
-    	rospy.spin()
-		
-
+    global x, y
+    
+    
+    rospy.init_node('last_target_node')
+    
+    
+    rospy.Subscriber('/last_target', PoseStamped, last_target_clbk)
+    
+   
+    rospy.Service('target_coord', target_coord, return_values)
+    
+    
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        rate.sleep()
 
 if __name__ == "__main__":
     main()
+
